26dce3b93e808e9f286b1c5499a2b7daea184250
greg <greg@influxdata.com>
2021-04-15 16:45:59 -0600

    chore: perform basic yaml linting

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,16 +3,16 @@
 CONTRACTS=${CONTRACTS:-contracts}
 
 # generate oss contract
-sed -e '/REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/oss.yml > src/.oss_gen.yml && \
-sed -i -e '/REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' src/.oss_gen.yml && \
-sed -i -e '/REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' src/.oss_gen.yml && \
+sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/oss.yml > src/.oss_gen.yml && \
+sed -i -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' src/.oss_gen.yml && \
+sed -i -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' src/.oss_gen.yml && \
 swagger-cli bundle src/.oss_gen.yml --outfile ${CONTRACTS}/oss.yml --type yaml && \
 rm src/.oss_gen.yml
 
 # generate cloud contract
-sed -e '/REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/cloud.yml > ./src/.cloud_gen.yml && \
-sed -i -e '/REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' src/.cloud_gen.yml && \
-sed -i -e '/REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' src/.cloud_gen.yml && \
+sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/cloud.yml > ./src/.cloud_gen.yml && \
+sed -i -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' src/.cloud_gen.yml && \
+sed -i -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' src/.cloud_gen.yml && \
 swagger-cli bundle src/.cloud_gen.yml --outfile ${CONTRACTS}/cloud.yml --type yaml && \
 rm src/.cloud_gen.yml
 
