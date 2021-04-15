c1f13da78f509cffec6719c2a50ed93d030c695c
greg <greg@influxdata.com>
2021-04-15 11:07:56 -0600

    chore: update to use true common bits to reduce maintenance burden

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -19,7 +19,13 @@ rm src/.cloud_gen.yml
 # generate cloud-priv contract
 swagger-cli bundle src/cloud-priv.yml --outfile ${CONTRACTS}/cloud-priv.yml --type yaml
 
+# generate common-only contract
+sed -e '/REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/common.yml > src/.common_gen.yml && \
+sed -i -e '/REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' src/.common_gen.yml && \
+sed -i -e '/REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' src/.common_gen.yml && \
+swagger-cli bundle src/.common_gen.yml --outfile ${CONTRACTS}/common.yml --type yaml && \
+rm src/.common_gen.yml
+
 # generate platform-specific contracts
-swagger-cli bundle src/common.yml --outfile ${CONTRACTS}/common.yml --type yaml
 swagger-cli bundle src/oss-diff.yml --outfile ${CONTRACTS}/oss-diff.yml --type yaml
 swagger-cli bundle src/cloud-diff.yml --outfile ${CONTRACTS}/cloud-diff.yml --type yaml
