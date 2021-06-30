c87a08f832e79bc338a37509028641cda9e1875b
Greg <2653109+glinton@users.noreply.github.com>
2021-06-30 08:43:09 -0600

    chore: add json swagger contracts (#15)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,6 +7,7 @@ sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/oss.yml |
 sed -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' |
 sed -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' > src/.oss_gen.yml && \
 swagger-cli bundle src/.oss_gen.yml --outfile ${CONTRACTS}/oss.yml --type yaml && \
+swagger-cli bundle src/.oss_gen.yml --outfile ${CONTRACTS}/oss.json --type json && \
 rm src/.oss_gen.yml
 
 # generate cloud contract
@@ -14,6 +15,7 @@ sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/cloud.yml |
 sed -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' |
 sed -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' > ./src/.cloud_gen.yml && \
 swagger-cli bundle src/.cloud_gen.yml --outfile ${CONTRACTS}/cloud.yml --type yaml && \
+swagger-cli bundle src/.cloud_gen.yml --outfile ${CONTRACTS}/cloud.json --type json && \
 rm src/.cloud_gen.yml
 
 # generate common-only contract
