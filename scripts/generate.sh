dc615e893e9252cd4749c92571577e98900b0111
Jason Stirnaman <jstirnaman@influxdata.com>
2021-09-14 14:12:48 -0500

    feat: docs URL placeholder replacement.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 CONTRACTS=${CONTRACTS:-contracts}
-INFLUXDB_DOCS_URL=https://docs.influxdb.com/influxdb
+INFLUXDB_DOCS_URL=https://docs.influxdata.com/influxdb
 
 # generate oss contract
 sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/oss.yml |
 sed -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' |
 sed -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' > src/.oss_gen.yml && \
 swagger-cli bundle src/.oss_gen.yml --type yaml | \
-(sed -e "s|{{% INFLUXDB_DOCS_URL %}}|${INFLUXDB_DOCS_URL}/v2.0|" > ${CONTRACTS}/oss.yml)  && \
+(sed -e "s|{{% INFLUXDB_DOCS_URL %}}|${INFLUXDB_DOCS_URL}/v2.0|g" > ${CONTRACTS}/oss.yml)  && \
 swagger-cli bundle ${CONTRACTS}/oss.yml --outfile ${CONTRACTS}/oss.yml --type yaml
 swagger-cli bundle src/.oss_gen.yml --outfile ${CONTRACTS}/oss.json --type json && \
 rm src/.oss_gen.yml
@@ -17,7 +17,9 @@ rm src/.oss_gen.yml
 sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/cloud.yml |
 sed -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' |
 sed -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' > ./src/.cloud_gen.yml && \
-swagger-cli bundle src/.cloud_gen.yml --outfile ${CONTRACTS}/cloud.yml --type yaml && \
+swagger-cli bundle src/.cloud_gen.yml --type yaml | \
+(sed -e "s|{{% INFLUXDB_DOCS_URL %}}|${INFLUXDB_DOCS_URL}/cloud|g" > ${CONTRACTS}/cloud.yml)  && \
+swagger-cli bundle ${CONTRACTS}/cloud.yml --outfile ${CONTRACTS}/cloud.yml --type yaml
 swagger-cli bundle src/.cloud_gen.yml --outfile ${CONTRACTS}/cloud.json --type json && \
 rm src/.cloud_gen.yml
 
@@ -25,7 +27,9 @@ rm src/.cloud_gen.yml
 sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/common.yml |
 sed -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' |
 sed -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' > src/.common_gen.yml && \
-swagger-cli bundle src/.common_gen.yml --outfile ${CONTRACTS}/common.yml --type yaml && \
+swagger-cli bundle src/.common_gen.yml --type yaml | \
+(sed -e "s|{{% INFLUXDB_DOCS_URL %}}|${INFLUXDB_DOCS_URL}/common|g" > ${CONTRACTS}/common.yml)  && \
+swagger-cli bundle ${CONTRACTS}/common.yml --outfile ${CONTRACTS}/common.yml --type yaml
 rm src/.common_gen.yml
 
 # generate platform-specific contracts
