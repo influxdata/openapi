6bc1e36a2262f64be16bf7597191b5612d2943c3
Jason Stirnaman <jstirnaman@influxdata.com>
2021-10-21 11:34:30 -0500

    fix: update extract-tags to get tags from the aggregated contract instead of sources.

--- scripts/extract-tags.sh
+++ scripts/extract-tags.sh
@@ -6,7 +6,7 @@
 export CUR_DIR=$(dirname "$0")
 
 echo '=== OSS ==='
-grep -r -A1 -e "tags:" -h $CUR_DIR/../src/common/paths/** $CUR_DIR/../src/oss/paths/** | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $CUR_DIR/../src/oss/tmp_tags.yml
+grep -A1 -e "tags:" -h $CUR_DIR/../contracts/ref/oss.yml | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $CUR_DIR/../src/oss/tmp_tags.yml
 echo '=== Cloud ==='
-grep -r -A1 -e "tags:" -h $CUR_DIR/../src/common/paths/** $CUR_DIR/../src/cloud/paths/** | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $CUR_DIR/../src/cloud/tmp_tags.yml
+grep -A1 -e "tags:" -h $CUR_DIR/../contracts/ref/cloud.yml | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $CUR_DIR/../src/cloud/tmp_tags.yml
 
