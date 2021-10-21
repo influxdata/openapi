78e0d1187b367ff6c7a3328d96719f5360d2f54d
Jason Stirnaman <jstirnaman@influxdata.com>
2021-10-21 17:50:17 -0500

    fix: tag cleanup

--- scripts/extract-tags.sh
+++ scripts/extract-tags.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+set -e
 
 # Extracts tags for listing in x-tagGroups.
 # Crude one-liner without YAML parsing.
 
 export CUR_DIR=$(dirname "$0")
 
-echo '=== OSS ==='
-grep -A1 -e "tags:" -h $CUR_DIR/../contracts/ref/oss.yml | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $CUR_DIR/../src/oss/tmp_tags.yml
-echo '=== Cloud ==='
-grep -A1 -e "tags:" -h $CUR_DIR/../contracts/ref/cloud.yml | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $CUR_DIR/../src/cloud/tmp_tags.yml
+oss_tags=$CUR_DIR/../src/oss/tmp_tags.yml
+cloud_tags=$CUR_DIR/../src/cloud/tmp_tags.yml
 
+grep -A1 -e "tags:" -h $CUR_DIR/../contracts/ref/oss.yml | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $oss_tags 
+echo "OSS: $oss_tags"
+grep -A1 -e "tags:" -h $CUR_DIR/../contracts/ref/cloud.yml | grep -e "- " | awk '{$1=$1};1' | uniq | sort > $cloud_tags 
+echo "Cloud: $cloud_tags"
