c94da29796704e16a9e8035b070d8370da2fe242
Jason Stirnaman <jstirnaman@influxdata.com>
2021-11-03 12:52:30 -0500

    fix: Strip version prefix from /legacy/authorizations

--- scripts/reference.sh
+++ scripts/reference.sh
@@ -19,9 +19,10 @@ swagrag \
   -file ${CONTRACTS}/oss.yml \
   -file ${CONTRACTS}/mapsd.yml \
   -api-title "Complete InfluxDB OSS API" \
+  | sed -e 's|^  /api/v2/health|  /health|' \
+  | sed -e 's|^  /api/v2/legacy|  /legacy|' \
   | sed -e 's|^  /api/v2/ping|  /ping|' \
   | sed -e 's|^  /api/v2/ready|  /ready|' \
-  | sed -e 's|^  /api/v2/health|  /health|' \
   > ${TCONTRACTS}/ref/oss.yml
 
 diff -r ${CONTRACTS}/ref ${TCONTRACTS}/ref/
