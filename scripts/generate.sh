b8ee023bd21c04c803bd959222f07afdf18845cc
Jason Stirnaman <jstirnaman@influxdata.com>
2021-11-05 11:28:18 -0500

    fix: update descriptions and bump docs oss URL to v2.1 (#233)
    * fix: placeholder cleanup in examples. Add links.
    
    * chore: update oss docs URL to v2.1
    
    fix: whitespace
    
    fix: typo.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,7 +10,7 @@ sed -e '/#REF_COMMON_PATHS/{r ./src/common/_paths.yml' -e 'd}' src/oss.yml |
 sed -e '/#REF_COMMON_PARAMETERS/{r ./src/common/_parameters.yml' -e 'd}' |
 sed -e '/#REF_COMMON_SCHEMAS/{r ./src/common/_schemas.yml' -e 'd}' > src/.oss_gen.yml && \
 swagger-cli bundle src/.oss_gen.yml --type yaml | \
-(sed -e "s|{{% INFLUXDB_DOCS_URL %}}|${INFLUXDB_DOCS_URL}/v2.0|g" > ${CONTRACTS}/oss.yml) && \
+(sed -e "s|{{% INFLUXDB_DOCS_URL %}}|${INFLUXDB_DOCS_URL}/v2.1|g" > ${CONTRACTS}/oss.yml) && \
 swagger-cli bundle ${CONTRACTS}/oss.yml --outfile ${CONTRACTS}/oss.yml --type yaml
 swagger-cli bundle src/.oss_gen.yml --outfile ${CONTRACTS}/oss.json --type json && \
 rm src/.oss_gen.yml
