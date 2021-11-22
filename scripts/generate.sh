748d0a64ed7031a334f3d48eba6fd9c4ad4dff9c
Roger Peppe <rogpeppe@gmail.com>
2021-11-22 13:32:30 +0000

    generation tweaks

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -20,23 +20,27 @@ SERVICES='
 	priv/svc-pinneditemsd
 '
 
-echo generating all >&2
+echo - generating all >&2
 # generate all the API definitions at once so we don't do the validation work many times
 (cd ./src && cue export -e '{"all": all}' -f -o $CONTRACTS/.all.yml)
-trap "rm $CONTRACTS/.all.yml" 0
+trap "rm -f $CONTRACTS/.all.yml" 0
 
 cd $CONTRACTS
 RESULTS=''
+echo - exporting API files >&2
 for c in $SERVICES; do
 	# TODO use a more consistent relationship between identifier
 	# and destination path so we don't need this logic.
 	id=$(basename $c)
 	out="./$(dirname $c)/$(echo $id | sed 's/^svc-//').yml"
-	echo generating $out >&2
 	cue export -e "all.\"$id\"" -f -o $out .all.yml
 	APIS="$APIS $out"
 done
 
+echo - validating generated APIs >&2
 for api in $APIS; do
-	swagger-cli validate $api
+	swagger-cli validate $api > /dev/null || {
+		echo $api failed validation >&2
+		echo >&2
+	}
 done
